d42a74cb639e2ee070659513764ac56d07af6132
James McIntyre <james.checks.his.email@gmail.com>
2016-01-19 11:42:59 -0800

    improvements to firebase data handling

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 //global vars
 var ref = new Firebase('https://jmmcontacts.firebaseio.com/');
 var questionsRef = ref.child('questions');
@@ -8,39 +7,44 @@ var $template;
 var $answerTemplate;
 
 $(document).ready(function() {
-  $('#test').click(addNewQuestion);
+  $('#addNewQuestion').click(addNewQuestion);
   $template = $('#cardtemplate');
   $answerTemplate = $('#answertemplate');
-  $(document).on('click', '#promptAnswerQuestion', showQ);
+  $(document).on('click', '#promptAnswerQuestion', showAnswerModal);
   $(document).on('click', '#addanswer', addNewAnswer);
+  $('#answerQuestionModal').on('hide.bs.modal', answerModalClosed);
 });
 
-
-
-function showQ(){
-  var questionKey = $(this).attr('data-key');
-  var retrievedQuestion = questionsRef.child(questionKey).on('value', function(snap){
-      var questionData = snap.val();
-      var question = questionData.question;
-      var date = questionData.date;
-      var currentAnswers = questionData.answers;
-      $('#questionTitle').text(question);
-      $('#questionDate').text("asked on "+date);
-      $('#addanswer').attr('data-key',questionKey);
-      var $domAnswers = [];
-
-      for (var prop in currentAnswers) {
-        var answerBody = currentAnswers[prop].answer;
-        var answerDate = currentAnswers[prop].date;
-        var $newAnswerDiv = $answerTemplate.clone().removeClass('template');
-        $newAnswerDiv.find('#answerbody').text(answerBody);
-        $newAnswerDiv.find('#answerdate').text(answerDate);
-        $domAnswers.push($newAnswerDiv);
-      }
-      $('#answerslist').append($domAnswers);
-  });
-
+function answerModalClosed(){
+  console.log('modal closed');
+  var currentKey = $('#addanswer').data('key');
+  questionsRef.child(currentKey).off();
 }
+
+function showAnswerModal(){
+  var questionKey = $(this).attr('data-key');
+  var currentQuestionRef = questionsRef.child(questionKey);
+
+  currentQuestionRef.on('value', function(snap){
+    var questionData = snap.val();
+
+    $('#questionTitle').text(questionData.question);
+    $('#questionDate').text("asked on " + questionData.date);
+    $('#addanswer').attr('data-key', questionKey);
+
+    currentQuestionRef.child('answers').once('value', function(answersSnap) {
+      var $domAnswers = [];
+      answersSnap.forEach(function(answerSnap){
+        var answerObj = answerSnap.val();
+        var $newAnswerDiv = $answerTemplate.clone().removeClass('template');
+        $newAnswerDiv.children('.answerbody').text(answerObj.answer);
+        $newAnswerDiv.children('.answerdate').text(answerObj.date);
+        $domAnswers.push($newAnswerDiv);
+      });
+      $('#answerslist').empty().append($domAnswers);
+    });
+  });
+};
 
 
 function addNewAnswer(){
@@ -48,8 +52,7 @@ function addNewAnswer(){
   var answer = $('#answerinput').val();
   var date = moment().format('MM/DD/YYYY h:mm a');
 
-  $('#newA')[0].reset();
-  $('#answerQuestionModal').modal('toggle');
+  $('#answerInput').val('');
 
   questionsRef.child(questionKey).child('answers').push({
     answer: answer,
@@ -59,13 +62,10 @@ function addNewAnswer(){
 
 
 function addNewQuestion(){
-
-  console.log('addNewQuestion was callled!!!!')
   var firstname = $('#firstname').val();
   var lastname = $('#lastname').val();
   var question = $('#questioninput').val();
   var date = moment().format('MM/DD/YYYY h:mm a');
-  var answers = [];
   $('#newQ')[0].reset();
   $('#addQuestionModal').modal('toggle');
 
@@ -73,31 +73,28 @@ function addNewQuestion(){
     firstname: firstname,
     lastname: lastname,
     question: question,
-    date: date,
-    answers: answers
+    date: date
   });
 }
-
 
 //LISTEN TO FIREBASE, REFLECT CHANGES
 
 questionsRef.on('value', function(snapshot) {
   var $domQuestions = [];
-    snapshot.forEach(function(childSnapshot) {
-        var thisChild = childSnapshot.val();
-        var key = childSnapshot.key();
-        var firstname = thisChild.firstname;
-        var lastname = thisChild.lastname;
-        var date = thisChild.date;
-        var question = thisChild.question;
-        var $newDiv = $template.clone().removeClass('template');
-        $newDiv.find('#name').text(firstname+" "+lastname);
-        $newDiv.find('#date').text(date);
-        $newDiv.find('#questionbody').text(question);
-        $newDiv.find('#promptAnswerQuestion').attr('data-key', key);
-        $domQuestions.push($newDiv);
-    });
+  snapshot.forEach(function(childSnapshot) {
+    var thisChild = childSnapshot.val();
+    var key = childSnapshot.key();
+    var firstname = thisChild.firstname;
+    var lastname = thisChild.lastname;
+    var date = thisChild.date;
+    var question = thisChild.question;
+    var $newDiv = $template.clone().removeClass('template');
+    $newDiv.find('#name').text(firstname+" "+lastname);
+    $newDiv.find('#date').text(date);
+    $newDiv.find('#questionbody').text(question);
+    $newDiv.find('#promptAnswerQuestion').attr('data-key', key);
+    $domQuestions.push($newDiv);
+  });
   $('#allCards').children().not('#newQuestionDiv').remove();
   $('#allCards').append($domQuestions);
-
 });
